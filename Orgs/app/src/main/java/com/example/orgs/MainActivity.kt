1f10682592c856a5d2d4c91b1698415a7d2340f4
wesley oliveira <wlwwesley9@gmail.com>
2022-09-17 11:55:58 -0300

    Iniciando minha Jornada

---
+++ Orgs/app/src/main/java/com/example/orgs/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.example.orgs
+
+import android.app.Activity
+import android.os.Bundle
+import android.widget.Toast
+
+class MainActivity: Activity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        Toast.makeText(this, "Bem Vindo ao Orgs", Toast.LENGTH_SHORT).show()
+    }
+
+}
