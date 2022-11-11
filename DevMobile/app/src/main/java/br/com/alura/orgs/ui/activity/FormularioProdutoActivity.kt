b28ad11f8d6c6bc48663d0f8a6d43a941c1381ab
wesley oliveira <wlwwesley9@gmail.com>
2022-11-10 21:05:11 -0300

    editando room

---
+++ DevMobile/app/src/main/java/br/com/alura/orgs/ui/activity/FormularioProdutoActivity.kt
@@ -0,0 +1,80 @@
+package br.com.alura.orgs.ui.activity
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import br.com.alura.orgs.database.AppDatabase
+import br.com.alura.orgs.databinding.ActivityFormularioProdutoBinding
+import br.com.alura.orgs.extensions.tentaCarregarImagem
+import br.com.alura.orgs.model.Produto
+import br.com.alura.orgs.ui.dialog.FormularioImagemDialog
+import java.math.BigDecimal
+
+class FormularioProdutoActivity : AppCompatActivity() {
+
+    private val binding by lazy {
+        ActivityFormularioProdutoBinding.inflate(layoutInflater)
+    }
+    private var url: String? = null
+    private var idProduto =0L
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+        title = "Cadastrar produto"
+        configuraBotaoSalvar()
+        binding.activityFormularioProdutoImagem.setOnClickListener {
+            FormularioImagemDialog(this)
+                .mostra(url) { imagem ->
+                    url = imagem
+                    binding.activityFormularioProdutoImagem.tentaCarregarImagem(url)
+                }
+        }
+        intent.getParcelableExtra<Produto>(CHAVE_PRODUTO)?.let{ produtoCarregado ->
+            title = "Alterar produto"
+            idProduto = produtoCarregado.id
+            url= produtoCarregado.imagem
+            binding.activityFormularioProdutoImagem.tentaCarregarImagem(produtoCarregado.imagem)
+            binding.activityFormularioProdutoNome.setText(produtoCarregado.nome)
+            binding.activityFormularioProdutoDescricao.setText(produtoCarregado.descricao)
+            binding.activityFormularioProdutoValor.setText(produtoCarregado.valor.toString())
+        }
+    }
+
+    private fun configuraBotaoSalvar() {
+        val botaoSalvar = binding.activityFormularioProdutoBotaoSalvar
+        val db = AppDatabase.instancia(this)
+        val produtoDao = db.produtoDao()
+        botaoSalvar.setOnClickListener {
+            val produtoNovo = criaProduto()
+            if(idProduto > 0){
+                produtoDao.altera(produtoNovo)
+            }else{
+                produtoDao.salva(produtoNovo)
+            }
+            finish()
+        }
+    }
+
+    private fun criaProduto(): Produto {
+        val campoNome = binding.activityFormularioProdutoNome
+        val nome = campoNome.text.toString()
+        val campoDescricao = binding.activityFormularioProdutoDescricao
+        val descricao = campoDescricao.text.toString()
+        val campoValor = binding.activityFormularioProdutoValor
+        val valorEmTexto = campoValor.text.toString()
+        val valor = if (valorEmTexto.isBlank()) {
+            BigDecimal.ZERO
+        } else {
+            BigDecimal(valorEmTexto)
+        }
+
+        return Produto(
+            id = idProduto,
+            nome = nome,
+            descricao = descricao,
+            valor = valor,
+            imagem = url
+        )
+    }
+
+}
